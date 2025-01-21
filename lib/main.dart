3b07c43c8266811a01d2669afddc5917352d496f
beck <beck@beckdeiMac.local>
2025-01-21 13:31:15 +0800

    dependency injection using get_it

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,16 @@
-import 'package:belog/core/secrets/app_secrets.dart';
 import 'package:belog/core/theme/theme.dart';
-import 'package:belog/features/auth/data/datasources/auth_remote_data_source.dart';
-import 'package:belog/features/auth/data/repositories/auth_repository_impl.dart';
-import 'package:belog/features/auth/domain/usecases/user_sign_up.dart';
 import 'package:belog/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:belog/features/auth/presentation/pages/login_page.dart';
+import 'package:belog/init_dependencies.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load(fileName: ".env");
-  final supabase = await Supabase.initialize(
-      url: AppSecrets.supabaseUrl, anonKey: AppSecrets.supabaseKey ?? '');
+
+  await initDependencies();
   runApp(MultiBlocProvider(
-    providers: [
-      BlocProvider(
-          create: (_) => AuthBloc(
-              userSignUp: UserSignUp(AuthRepositoryImpl(
-                  AuthRemoteDataSourceImpl(supabase.client)))))
-    ],
+    providers: [BlocProvider(create: (_) => serviceLocator<AuthBloc>())],
     child: const MyApp(),
   ));
 }
